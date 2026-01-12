a1e7b206a7b2284af96c3656a88bddeb8861bda7
AxSRockS <axsrocks@gmail.com>
2026-01-12 14:42:54 -0300

    Add admin user to DatabaseSeeder for persistence across deploys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,23 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Create admin user (idempotent - won't duplicate)
+        User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+
         $this->call([
             ClientSeeder::class,
             SupplierSeeder::class,
